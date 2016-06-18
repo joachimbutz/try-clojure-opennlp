3dff278564dee938798bcb502af7eb9493b1c384
Joachim Butz <joachim.butz@gmx.at>
2016-06-18 13:50:07 +0200

    initial commit

---
+++ test/try_clojure_opennlp/core_test.clj
@@ -0,0 +1,7 @@
+(ns try-clojure-opennlp.core-test
+  (:require [clojure.test :refer :all]
+            [try-clojure-opennlp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
